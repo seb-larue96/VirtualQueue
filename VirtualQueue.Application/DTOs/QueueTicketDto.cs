bca66001ea9133b89a86358278b0a2776a6b274f
seb-larue96 <larue.sebastien@dsi-oi.com>
2026-01-30 07:53:24 +0400

    configure queuu ticket dto and interface

---
+++ VirtualQueue.Application/DTOs/QueueTicketDto.cs
@@ -0,0 +1,11 @@
+﻿using VirtualQueue.Domain.Enums;
+
+namespace VirtualQueue.Application.DTOs;
+
+public record QueueTicketDto(
+    Guid Id,
+    int Number,
+    string CustomerName,
+    DateTime JoinedAt,
+    TicketStatus Status
+);
---
+++ VirtualQueue.Application/Interfaces/IQueueService.cs
@@ -0,0 +1,11 @@
+﻿using VirtualQueue.Application.DTOs;
+
+namespace VirtualQueue.Application.Interfaces;
+
+public interface IQueueService
+{
+    QueueTicketDto Enqueue(string customerName);
+    QueueTicketDto? CallNext();
+    IReadOnlyList<QueueTicketDto> GetQueue();
+    int GetPosition(Guid ticketId);
+}
